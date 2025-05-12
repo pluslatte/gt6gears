681c5b0a201af62e547aad6038bba2a1982400cc
pluslatte <debrisfield0201@gmail.com>
2025-05-12 21:54:07 +0900

    basic setup maybe

--- src/main/java/com/pluslatte/gtgear/GtGear.java
+++ src/main/java/com/pluslatte/gtgear/GtGear.java
@@ -1,21 +1,86 @@
 package com.pluslatte.gtgear;
 
+import cpw.mods.fml.common.event.*;
 import gregapi.api.Abstract_Mod;
+import gregapi.api.Abstract_Proxy;
+import gregapi.code.ModData;
 import net.minecraft.init.Blocks;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
 
 @Mod(modid = GtGear.MODID, version = GtGear.VERSION)
 public final class GtGear extends Abstract_Mod
 {
     public static final String MODID = "gtgear";
+    public static final String MODNAME = "GtGear";
     public static final String VERSION = "1.0";
+    public static ModData MOD_DATA = new ModData(MODID, MODNAME);
 
-    @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
+    @cpw.mods.fml.common.SidedProxy(modId = MODID, clientSide = "gregapi.api.example.Example_Proxy_Client", serverSide = "gregapi.api.example.Example_Proxy_Server")
+    public static gregapi.api.Abstract_Proxy PROXY;
+
+    @Override
+    public String getModID() {
+        return MODID;
+    }
+
+    @Override
+    public String getModName() {
+        return MODNAME;
+    }
+
+    @Override
+    public String getModNameForLog() {
+        return MODNAME;
+    }
+
+    @Override
+    public Abstract_Proxy getProxy() {
+        return PROXY;
+    }
+
+    // Do not change these 7 Functions. Just keep them this way.
+    @cpw.mods.fml.common.Mod.EventHandler public final void onPreLoad           (cpw.mods.fml.common.event.FMLPreInitializationEvent    aEvent) {onModPreInit(aEvent);}
+    @cpw.mods.fml.common.Mod.EventHandler public final void onLoad              (cpw.mods.fml.common.event.FMLInitializationEvent       aEvent) {onModInit(aEvent);}
+    @cpw.mods.fml.common.Mod.EventHandler public final void onPostLoad          (cpw.mods.fml.common.event.FMLPostInitializationEvent   aEvent) {onModPostInit(aEvent);}
+    @cpw.mods.fml.common.Mod.EventHandler public final void onServerStarting    (cpw.mods.fml.common.event.FMLServerStartingEvent       aEvent) {onModServerStarting(aEvent);}
+    @cpw.mods.fml.common.Mod.EventHandler public final void onServerStarted     (cpw.mods.fml.common.event.FMLServerStartedEvent        aEvent) {onModServerStarted(aEvent);}
+    @cpw.mods.fml.common.Mod.EventHandler public final void onServerStopping    (cpw.mods.fml.common.event.FMLServerStoppingEvent       aEvent) {onModServerStopping(aEvent);}
+    @cpw.mods.fml.common.Mod.EventHandler public final void onServerStopped     (cpw.mods.fml.common.event.FMLServerStoppedEvent        aEvent) {onModServerStopped(aEvent);}
+
+    @Override
+    public void onModPreInit2(FMLPreInitializationEvent aEvent) {
+
+    }
+
+    @Override
+    public void onModInit2(FMLInitializationEvent aEvent) {
         // some example code
         System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+    }
+
+    @Override
+    public void onModPostInit2(FMLPostInitializationEvent aEvent) {
+
+    }
+
+    @Override
+    public void onModServerStarting2(FMLServerStartingEvent aEvent) {
+
+    }
+
+    @Override
+    public void onModServerStarted2(FMLServerStartedEvent aEvent) {
+
+    }
+
+    @Override
+    public void onModServerStopping2(FMLServerStoppingEvent aEvent) {
+
+    }
+
+    @Override
+    public void onModServerStopped2(FMLServerStoppedEvent aEvent) {
+
     }
 }

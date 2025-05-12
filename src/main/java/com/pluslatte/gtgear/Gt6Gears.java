24ac417e04b0e87dd9aa0cd07a1f281aa365696a
pluslatte <debrisfield0201@gmail.com>
2025-05-12 22:58:54 +0900

    change name

--- src/main/java/com/pluslatte/gtgear/Gt6Gears.java
+++ src/main/java/com/pluslatte/gtgear/Gt6Gears.java
@@ -15,7 +15,7 @@ import net.minecraft.item.ItemStack;
 public final class Gt6Gears extends Abstract_Mod
 {
     public static final String MODID = "gt6gears";
-    public static final String MODNAME = "pluslatte's gt6 gears";
+    public static final String MODNAME = "gt6 gears";
     public static final String VERSION = "1.0";
     public static ModData MOD_DATA = new ModData(MODID, MODNAME);
 
